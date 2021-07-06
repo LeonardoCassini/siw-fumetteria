c04678a720cd3d18f7d21d9cc7f8c991587d5dd0
lios <leo.cassini@stud.uniroma3.it>
2021-07-06 20:01:13 +0200

    sistemato il problema della ricerca dei fumettisti e dei mangaka

--- siw-fumetteria/src/main/java/it/uniroma3/siw/spring/controller/AutoreController.java
+++ siw-fumetteria/src/main/java/it/uniroma3/siw/spring/controller/AutoreController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 //import it.uniroma3.siw.spring.model.Autore;
 //import org.springframework.web.bind.annotation.RestController;
 import it.uniroma3.siw.spring.service.AutoreService;
@@ -16,16 +18,10 @@ public class AutoreController
 	@Autowired
 	private AutoreService autoreService;
 	
-	@RequestMapping("/autori")
-	public String autori() 
-	{
-	    return "autori";
-	}
-	
-	@RequestMapping("/autoreStruttura")
+	@RequestMapping(value="/autoreStruttura")
 	public String autoreStruttura()
 	{
-		return "autoreStruttura";
+		return"/autoreStruttura";
 	}
 	
 	@RequestMapping(value="/autori", method= RequestMethod.GET)
@@ -42,17 +38,32 @@ public class AutoreController
 		return "opereAutore.html";
 	}
 	
-	@RequestMapping(value="/autoreStruttura",method=RequestMethod.GET)
-	public String showMangaka(int value,Model model)
+//	@RequestMapping(value="/trovaArtista",method=RequestMethod.GET,params="action=77")
+//	public String showMangaka(Model model)
+//	{
+//		model.addAttribute("autori",this.autoreService.getMangaka());
+//		return"/autori";
+//	}
+//	
+//	@RequestMapping(value="/trovaArtista",method=RequestMethod.GET,params="action=67")
+//	public String showFumettisti(Model model)
+//	{
+//		model.addAttribute("autori",this.autoreService.getFumettista());
+//		return"/autori";
+//	}
+	
+	
+	@RequestMapping(value="/trovaArtista",method=RequestMethod.GET)
+	public String showFumettistiMangaka(Model model,@RequestParam(value="action",required=true)int valore)
 	{
-		if(value==67)
+		if(valore==77)
 		{
 			model.addAttribute("autori",this.autoreService.getMangaka());
 		}
-		else
+		else if(valore==67)
 		{
 			model.addAttribute("autori",this.autoreService.getFumettista());
 		}
-		return"/autoreStruttura";
+		return"/autori";
 	}
 }
